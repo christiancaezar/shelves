947ab417baf9226ea60654546b408373bc63cfc9
Ian Cezar Ortega <christiancaezar@gmail.com>
2022-12-07 21:42:59 +0800

    gudjab

--- src/app/pages/login-main/login-main.page.ts
+++ src/app/pages/login-main/login-main.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-main',
   templateUrl: './login-main.page.html',
   styleUrls: ['./login-main.page.scss'],
 })
+
 export class LoginMainPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
 
   ngOnInit() {
+  }
+  
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Email or Password Error',
+      subHeader: 'Hey, your email or password might be wrong.',
+      message: 'You can try again though.',
+      buttons: ['Ok, thank you.'],
+    });
+
+    await alert.present();
   }
 
 }
