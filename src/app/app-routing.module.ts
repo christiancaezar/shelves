1c3525924d273f3e744be0f1ccab1a3368fd037f
Ian Cezar Ortega <christiancaezar@gmail.com>
2022-12-07 20:55:44 +0800

    gudjab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'login-main',
+    loadChildren: () => import('./pages/login-main/login-main.module').then( m => m.LoginMainPageModule)
+  },
+  {
+    path: 'sign-up',
+    loadChildren: () => import('./pages/sign-up/sign-up.module').then( m => m.SignUpPageModule)
   }
 ];
 
---
+++ src/app/pages/login-main/login-main.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-main',
+  templateUrl: './login-main.page.html',
+  styleUrls: ['./login-main.page.scss'],
+})
+export class LoginMainPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
